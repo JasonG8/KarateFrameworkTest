387af3b4d4e23f221b940786ce02eb57812953a9
JasonG8 <jdguzman428@misena.edu.co>
2022-11-09 15:06:39 -0500

    Generar reportes en cucumber

--- src/test/java/REST/KarateRunner.java
+++ src/test/java/REST/KarateRunner.java
@@ -1,9 +1,12 @@
+package REST;
+
 import com.intuit.karate.junit5.Karate;
 
 public class KarateRunner {
 
     @Karate.Test
     Karate testSample() {
-        return Karate.run("classpath:apiREST/KarateTest.feature").tags("@casoPost");
+        return Karate.run("classpath:REST/KarateTest.feature").tags("@casoPost");
     }
+
 }
